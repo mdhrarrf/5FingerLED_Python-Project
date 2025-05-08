05f93850e09fa277d701667de0b725afaa5d2114
mdhrarrf <haidarmeeteng@gmail.com>
2025-05-08 07:37:41 +0700

    terbaru

--- controller.py
+++ controller.py
@@ -1,22 +1,51 @@
 import pyfirmata
 
-comport = 'COM9'  # Ganti jika bukan COM9
-board = pyfirmata.Arduino(comport)
+comport='COM9'
 
-# LED 1–10: kanan = pin 8–12, kiri = pin 3–7
-led_pins = [
-    board.get_pin('d:8:o'),   # LED 1 (kanan)
-    board.get_pin('d:9:o'),   # LED 2
-    board.get_pin('d:10:o'),  # LED 3
-    board.get_pin('d:11:o'),  # LED 4
-    board.get_pin('d:12:o'),  # LED 5
-    board.get_pin('d:7:o'),   # LED 6 (kiri)
-    board.get_pin('d:6:o'),   # LED 7
-    board.get_pin('d:5:o'),   # LED 8
-    board.get_pin('d:4:o'),   # LED 9
-    board.get_pin('d:3:o'),   # LED 10
-]
+board=pyfirmata.Arduino(comport)
+
+
+led_1=board.get_pin('d:8:o')
+led_2=board.get_pin('d:9:o')
+led_3=board.get_pin('d:10:o')
+led_4=board.get_pin('d:11:o')
+led_5=board.get_pin('d:12:o')
 
 def led(fingerUp):
-    for i in range(10):
-        led_pins[i].write(fingerUp[i])
+    if fingerUp==[0,0,0,0,0]:
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(0)
+
+    elif fingerUp==[0,1,0,0,0]:
+        led_1.write(1)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(0)
+    elif fingerUp==[0,1,1,0,0]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(0)    
+    elif fingerUp==[0,1,1,1,0]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(0)
+        led_5.write(0)
+    elif fingerUp==[0,1,1,1,1]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(1)
+        led_5.write(0)
+    elif fingerUp==[1,1,1,1,1]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(1)
+        led_5.write(1)
